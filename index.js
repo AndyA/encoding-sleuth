b05edd22538da46d13f4c7b6237100601b2ca301
Andy Armstrong <andy@hexten.net>
2019-05-20 22:55:25 +0100

    Refactor moar.

--- index.js
+++ index.js
@@ -51,29 +51,29 @@ class EncodingSleuth {
 
     function utf8cp() {
       let cp = 0;
-      let extra;
+      let length;
 
       const c = buf[pos];
 
       if ((c & 0xfe) === 0xfc) {
-        [cp, extra] = [c & 0x01, 5];
+        [cp, length] = [c & 0x01, 6];
       } else if ((c & 0xfc) === 0xf8) {
-        [cp, extra] = [c & 0x03, 4];
+        [cp, length] = [c & 0x03, 5];
       } else if ((c & 0xf8) === 0xf0) {
-        [cp, extra] = [c & 0x07, 3];
+        [cp, length] = [c & 0x07, 4];
       } else if ((c & 0xf0) === 0xe0) {
-        [cp, extra] = [c & 0x0f, 2];
+        [cp, length] = [c & 0x0f, 3];
       } else if ((c & 0xe0) === 0xc0) {
-        [cp, extra] = [c & 0x1f, 1];
+        [cp, length] = [c & 0x1f, 2];
       } else {
         return false;
       }
 
       // End of buffer?
-      if (pos + 1 + extra > len)
+      if (pos + length > len)
         return false;
 
-      for (let i = 1; i <= extra; i++) {
+      for (let i = 1; i < length; i++) {
         const cc = buf[pos + i];
         if ((cc & 0xc0) !== 0x80)
           return false;
@@ -88,19 +88,19 @@ class EncodingSleuth {
       if (!opt.allowSpecials && cp >= 0xfff0 && cp < 0x10000)
         return false;
 
-      // Illegal code points
+      // Illegal code points?
       if (!opt.allowIllegalCodePoints && cp >= 0xd800 && cp < 0xe000)
         return false;
 
-      // Max code point
+      // Max code point?
       if (opt.maxCodePoint !== false && cp >= opt.maxCodePoint)
         return false;
 
       // Inefficient encoding?
-      if (!opt.allowInefficientEncoding && cp < Math.max(0x80, 1 << (extra * 5 + 1)))
+      if (!opt.allowInefficientEncoding && cp < Math.max(0x80, 1 << (length * 5 - 4)))
         return false;
 
-      pos += 1 + extra;
+      pos += length;
       return cp;
     }
 
