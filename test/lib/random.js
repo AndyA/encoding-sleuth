e6b4c502d0c196fce6d0014afbf3eec34a8419d8
Andy Armstrong <andy@hexten.net>
2019-05-22 17:29:37 +0100

    Deterministic random numbers.

--- test/lib/random.js
+++ test/lib/random.js
@@ -7,13 +7,58 @@ class Random {
     }, opt || {});
   }
 
+  // From
+  //   https://stackoverflow.com/questions/521295/ \
+  //     seeding-the-random-number-generator-in-javascript/47593316#47593316
+  // 
+  static sfc32(ra, rb, rc, rd) {
+    return () => {
+      ra >>>= 0;
+      rb >>>= 0;
+      rc >>>= 0;
+      rd >>>= 0;
+      let tmp = (ra + rb) | 0;
+      ra = rb ^ rb >>> 9;
+      rb = rc + (rc << 3) | 0;
+      rc = (rc << 21 | rc >>> 11);
+      rd = rd + 1 | 0;
+      tmp = tmp + rd | 0;
+      rc = rc + tmp | 0;
+      return (tmp >>> 0) / 4294967296;
+    }
+  }
+
+  static seed(ra, rb, rc, rd) {
+    if (ra === undefined)
+      ra = 0;
+    if (rb === undefined)
+      rb = (ra ^ -1) >>> 0;
+    if (rc === undefined)
+      rc = (ra ^ rb) >>> 0;
+    if (rd === undefined)
+      rd = rc;
+
+    this.rawRandom = this.sfc32(ra, rb, rc, rd);
+
+    // Warm it up
+    for (let i = 0; i < 20; i++)
+      this.rawRandom();
+  }
+
+  static randomise() {
+    this.seed(Math.random() * 4294967296);
+  }
+
   random() {
-    return Math.pow(Math.random(), this.opt.pow);
+    return Math.pow(this.constructor.rawRandom(), this.opt.pow);
   }
 
   randomBetween(low, high) {
     return Math.floor(this.random() * (high - low) + low);
   }
 }
+
+Random.randomize = Random.randomise;
+Random.randomize();
 
 module.exports = Random;
