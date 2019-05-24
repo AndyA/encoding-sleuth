c3325e9ad091fbd2b5b7782e232c5437faa88540
Andy Armstrong <andy@hexten.net>
2019-05-24 10:26:07 +0100

    More example

--- examples/runkit.js
+++ examples/runkit.js
@@ -4,9 +4,15 @@ const es = new EncodingSleuth();
 
 // Analyse some bytes
 const span = Array.from(es.analyse(Uint8Array.from([
-  65, 66, 67,  // ABC
+  65, 66, 67, // ABC
   200, // unknown
-  220, 129 // utf8
+  220, 129, // utf8
+  248, 128, 128, 156, 129, // non-canonical
+  237, 160, 131, // illegal 
+  244, 163, 145, 150, // above max
 ])));
 
 console.log(span);
+
+
+
