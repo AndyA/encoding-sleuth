f0a8a24992f36a264f313d05acb5bb3f9eeee147
Andy Armstrong <andy@hexten.net>
2019-05-24 10:13:07 +0100

    Add runkit example.

---
+++ examples/runkit.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const EncodingSleuth = require("encoding-sleuth");
+
+const es = new EncodingSleuth();
+
+// Analyse some bytes
+const span = Array.from(es.analyse(Uint8Array.from([
+  65, 66, 67,  // ABC
+  200, // unknown
+  220, 129 // utf8
+])));
+
+console.log(span);
